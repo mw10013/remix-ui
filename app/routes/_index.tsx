881cb0be0678c45eea20f7d66693ff2a579d66e7
Michael Wu <mw10013@gmail.com>
2024-08-12 16:26:33 -0400

    scn init

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,12 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
+// import { Button as ScnButton } from "~/lib/components/ui/button"
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "remix-ui" },
     {
       name: "description",
-      content: "Welcome to Remix on Cloudflare!",
+      content: "Welcome to remix-ui on Cloudflare!",
     },
   ];
 };
@@ -13,29 +14,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix on Cloudflare</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-      </ul>
+      <h1 className="text-3xl">Welcome to remix-ui on Cloudflare</h1>
     </div>
   );
 }
