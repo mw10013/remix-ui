4e9671701550d71f40b415c805f13be2f7835e70
Michael Wu <mw10013@gmail.com>
2024-08-12 20:23:18 -0400

    .

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,8 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
 import { Button as ScnButton } from "~/lib/components/ui/button";
 import { ChevronRight, Mail, Loader2 } from "lucide-react";
-import { Button } from "~/lib/components/rac-starter/Button";
+import { Button as RacButton } from "~/lib/components/rac-starter/Button";
+import { Button } from "~/lib/components/rac/Button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -15,13 +16,17 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
+    <div className="font-sans p-4 flex flex-col gap-2">
       <h1 className="text-3xl">Welcome to remix-ui on Cloudflare</h1>
       <div className="flex flex-col gap-4 max-w-[175px]">
-        <Button>Rac Button</Button>
+        <RacButton variant="primary">Rac Button</RacButton>
+        <RacButton variant="secondary">Rac Button</RacButton>
+        <RacButton variant="destructive">Rac Button</RacButton>
+        <ChevronRight className="h-4 w-4" />
+        <RacButton variant="icon"></RacButton>
       </div>
       <div className="flex flex-col gap-4 max-w-[175px]">
-        <ScnButton>Button</ScnButton>
+        <ScnButton disabled>Button</ScnButton>
         <ScnButton variant="secondary">Secondary</ScnButton>
         <ScnButton variant="destructive">Destructive</ScnButton>
         <ScnButton variant="outline">Outline</ScnButton>
@@ -37,6 +42,19 @@ export default function Index() {
           <Loader2 className="mr-2 h-4 w-4 animate-spin" />
           Please wait
         </ScnButton>
+      </div>
+      <div className="flex justify-center mt-12">
+        <div className="grid grid-cols-3 gap-4">
+          <Button>Button 1</Button>
+          <Button>Button 2</Button>
+          <Button>Button 3</Button>
+          <Button>Button 4</Button>
+          <Button>Button 5</Button>
+          <Button>Button 6</Button>
+          <Button>Button 7</Button>
+          <Button>Button 8</Button>
+          <Button>Button 9</Button>
+        </div>
       </div>
     </div>
   );
