44e76e92735653b9611207c6da0874b469453240
Michael Wu <mw10013@gmail.com>
2024-08-25 21:38:13 -0400

    .

--- app/routes/input.tsx
+++ app/routes/input.tsx
@@ -1,3 +1,4 @@
+import { TextField } from '~/lib/components/rac/TextField'
 import { Input as ShadcnInput } from '~/lib/components/ui/input'
 import { Label as ShadcnLabel } from '~/lib/components/ui/label'
 
@@ -5,6 +6,9 @@ export default function RouteComponent() {
   return (
     <div className="container flex flex-col gap-2 p-6">
       Input
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <TextField label="Email" />
+      </div>
       <div className="grid w-full max-w-sm items-center gap-1.5">
         <ShadcnLabel htmlFor="email">Email</ShadcnLabel>
         <ShadcnInput type="email" id="email" placeholder="Email" />
